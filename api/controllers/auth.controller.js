bac5dc2b6322c2dde5c94b47ad5feae47672ac05
Christopher <mwanikigachina@gmail.com>
2024-02-02 17:15:43 +0300

    updated auth controller

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+import User from "../models/user.modal.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+    const { username, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save()
+    res.status(201).json("User created successfully!");
+}
