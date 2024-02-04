20b58455407634ede7db4a0b9a6f6bbc27cc8e24
Christopher <mwanikigachina@gmail.com>
2024-02-04 13:27:48 +0300

    create a middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -15,13 +15,22 @@ mongoose
     })
 
 const app = express();
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
 
 app.use(express.json());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
 });
 
