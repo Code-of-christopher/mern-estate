ca89048a0fb6c6fc62f8f18f9afc3c11a74c4c2f
Christopher <mwanikigachina@gmail.com>
2024-02-13 21:40:00 +0300

    update the header and make the profile page private

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -35,7 +35,6 @@ export const signin = async ( req, res, next ) => {
     }
 }
 
-
 export const google = async(req, res, next) => {
     try {
         const user = await User.findOne({email: req.body.email})
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn'
 import Profile from './pages/Profile'
 import About from './pages/About'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 
 export default function App() {
   return <BrowserRouter>
@@ -14,7 +15,9 @@ export default function App() {
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/about" element={<About />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route element={<PrivateRoute />} >
+        <Route path="/profile" element={<Profile />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 }
