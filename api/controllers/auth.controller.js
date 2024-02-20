5ae65655d9f58f047cd61f2ffa9708bd9afe1a22
Christopher <mwanikigachina@gmail.com>
2024-02-20 20:27:07 +0300

    create user update api router

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import User from "../models/user.modal.js";
+import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import jwt from 'jsonwebtoken';
@@ -25,7 +25,7 @@ export const signin = async ( req, res, next ) => {
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if (!validPassword) return next(errorHandler(401, 'Wrong credentials'));
         const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET);
-        const { password: pass, ...rest } = validUser._doc;
+        const { password: pass, ...rest } = validUser;
         res
             .cookie('access_token', token, { httpOnly: true})
             .status(200)
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -35,6 +35,7 @@ export default function OAuth() {
             type='button'
             className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'
         >
-            Continue with google</button>
+            Continue with google
+        </button>
     )
 }
